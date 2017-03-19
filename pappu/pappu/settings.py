96cbe48ba67bdd7e9a81130f16d3f8273a27c729
Shubham Aggarwal <shubham.aggarwal2020@gmail.com>
2017-03-20 01:32:55 +0530

    test

--- pappu/pappu/settings.py
+++ pappu/pappu/settings.py
@@ -51,8 +51,8 @@ WSGI_APPLICATION = 'pappu.wsgi.application'
 
 DATABASES = {
      'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.sqlite3',
      }
 }
 
